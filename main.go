6fb2dfa705e2b4b4eb102f41857d29d326c7bffb
Michelle <michlee1337@gmail.com>
2021-08-24 16:52:11 -0400

    take debug flag to generate commented code

--- main.go
+++ main.go
@@ -3,31 +3,38 @@ package main
 import (
 	"fmt"
 	"os"
+	"flag"
 	"strings"
 	"github.com/michlee1337/vm-translator/parser"
 	"github.com/michlee1337/vm-translator/coder"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		panic("vm translator should be called with an argument as follows: vm-translator [file-to-translate.asm]")
+	debugPtr := flag.Bool("debug", false, "Generates .asm files that are more readable for debugging")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
+		panic("vm translator should be called with an argument as follows: vm-translator [-debug] [file-to-translate.asm]")
 	}
 
-	infile, err := os.Open(os.Args[1])
+	infile, err := os.Open(args[0])
 	if err != nil {
 		panic("failed to open file")
 	}
 
-	file_name := os.Args[1][:strings.Index(os.Args[1], ".vm")]
-	outfile, err := os.Create(fmt.Sprintf("%v.asm", file_name))
+	file := args[0][:strings.Index(args[0], ".vm")]
+	outfile, err := os.Create(fmt.Sprintf("%v.asm", file))
 	if err != nil {
 		panic("failed to create file")
 	}
 	defer outfile.Close()
 
 	p := parser.New(infile)
-	c := coder.New(file_name)
-
+	c := coder.New(file, *debugPtr)
+	
+	if *debugPtr {
+		outfile.WriteString("// This file was generated in debug mode\n")
+	}
 	for p.Advance() {
 		switch p.CommandType() {
 			case parser.CPush:
